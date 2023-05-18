078ec8e7a6dac998b1d63446f5c0b327480e7e43
jobobdev <tmvle22@gmail.com>
2023-05-18 20:36:29 +0900

    up to #2.11

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,18 +2,82 @@ const socket = io(); //io function will automatically find the server that socke
 
 const welcom = document.getElementById("welcome");
 const form = document.querySelector("form");
+const room = document.getElementById("room");
 
-function backendDone(msg) {
-  console.log("The Backend Says:", msg);
+room.hidden = true;
+let roomName = "";
+
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
+function showRoom() {
+  welcom.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#msg");
+  const nicknameForm = room.querySelector("#nickname");
+  msgForm.addEventListener("submit", handleMessageSubmit);
+  nicknameForm.addEventListener("submit", handleNicknameSubmit);
+}
+
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#nickname input");
+  const value = input.value;
+  socket.emit("nickname", value);
+  input.value = "";
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, backendDone);
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 //socket.emit can emit any event(here we named 'enter_room') we want. and we can even send JSON directly to the server. and the third argument is the callback function we can call from our SERVER, that is on the front-end!!! In this case the function called done() set in the backend will be triggerd here.
 //in socket.emit, you can send several arguments you want to. you can send anything.
 //if you want to send the function to be executed, it has to be the last object.
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome event", (user, newCount) => {
+  addMessage(`${user} joined!`);
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName} (${newCount})`;
+});
+
+socket.on("bye", (left, newCount) => {
+  addMessage(`${left} left`);
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}  (${newCount})`;
+});
+
+socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+  const roomList = welcome.querySelector("ul");
+  roomList.innerHTML = "";
+  if (rooms.length === 0) {
+    return;
+  }
+  rooms.forEach((room) => {
+    const li = document.createElement("li");
+    li.innerText = room;
+    roomList.append(li);
+  });
+}); //=== socket.on("room_change", (msg) => console.log(msg));
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import http from "http";
 // import WebSocket from "ws";
+
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
-import { Socket } from "dgram";
 
 const app = express();
 
@@ -11,20 +12,65 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/ "));
-
-const handlelisten = () => console.log(`Listeneing on http://localhost:3000`); //express handles http
+app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app); //this gives me the server
-const ioServer = new Server(httpServer);
+const ioServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+instrument(ioServer, {
+  auth: false,
+  mode: "development",
+});
+
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = ioServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return ioServer.sockets.adapter.rooms.get(roomName)?.size;
+}
 
 ioServer.on("connection", (socket) => {
-  socket.on("enter_room", (roomName, done) => {
-    console.log(roomName);
-    setTimeout(() => {
-      done("Yo I am the Backend!");
-    }, 10000);
+  socket["nickname"] = "Anonymous";
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
   });
+  socket.on("enter_room", (roomName, roomCreated) => {
+    socket.join(roomName);
+    roomCreated();
+    socket
+      .to(roomName)
+      .emit("welcome event", socket.nickname, countRoom(roomName));
+    ioServer.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+    );
+  });
+  socket.on("disconnect", () => {
+    ioServer.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+    done();
+  });
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 //done() does not run the backendDone() function at the backend. instead, it is just like pressing 'play' button that triggers backendDone() to be executed in the front-end.
 //and the done() can even send the argument.
@@ -55,5 +101,5 @@ wss.on("connection", (socket) => {
 
 //wss.on waits the event to happen, then the function is called. also it gives us the info of someone who just connected to our backend which is through the 'socket'.
 */
-
-httpServer.listen(3000, handlelisten);
+const handlelisten = () => console.log(`Listeneing on http://localhost:9000`); //express handles http
+httpServer.listen(9000, handlelisten);
