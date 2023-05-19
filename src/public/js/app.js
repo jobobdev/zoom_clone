9cca1bde346a08147836f7cf4ba0054072780acc
jobobdev <tmvle22@gmail.com>
2023-05-19 16:16:13 +0900

    icecandidate pingpong error occured, #3.7

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,83 +1,149 @@
 const socket = io(); //io function will automatically find the server that socket.io is running on => this connects the socket.io to the front-end
 
-const welcom = document.getElementById("welcome");
-const form = document.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteButton = document.getElementById("mute");
+const cameraButton = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
-room.hidden = true;
+const call = document.getElementById("call");
+
+call.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
 let roomName = "";
+let myPeerConnection;
 
-function addMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+    console.log(cameras);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function handleMessageSubmit(event) {
+async function getMedia(deviceId) {
+  const initialConstraints = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function handleMute() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteButton.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteButton.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCameraOff() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraButton.innerText = "Camera Off";
+    cameraOff = false;
+  } else {
+    cameraButton.innerText = "Camera On";
+    cameraOff = true;
+  }
+}
+
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
+muteButton.addEventListener("click", handleMute);
+cameraButton.addEventListener("click", handleCameraOff);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form
+const welcome = document.getElementById("welcom");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+async function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_message", input.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
-}
-
-function showRoom() {
-  welcom.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-  const nicknameForm = room.querySelector("#nickname");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-  nicknameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-function handleNicknameSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#nickname input");
-  const value = input.value;
-  socket.emit("nickname", value);
-  input.value = "";
-}
-
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
   roomName = input.value;
   input.value = "";
 }
-//socket.emit can emit any event(here we named 'enter_room') we want. and we can even send JSON directly to the server. and the third argument is the callback function we can call from our SERVER, that is on the front-end!!! In this case the function called done() set in the backend will be triggerd here.
-//in socket.emit, you can send several arguments you want to. you can send anything.
-//if you want to send the function to be executed, it has to be the last object.
-form.addEventListener("submit", handleRoomSubmit);
+welcomeForm.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome event", (user, newCount) => {
-  addMessage(`${user} joined!`);
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-});
+// Socket Code
 
-socket.on("bye", (left, newCount) => {
-  addMessage(`${left} left`);
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}  (${newCount})`;
-});
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+}); //runs on peer A, when B joins. it creates offer and sends it to the server
 
-socket.on("new_message", addMessage);
+socket.on("offer", async (offer) => {
+  console.log("Recieved offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setRemoteDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("Sent the answer");
+}); // runs on peer B, when B receives the offer from the server. and created Answer for that.
 
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if (rooms.length === 0) {
-    return;
-  }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(li);
-  });
-}); //=== socket.on("room_change", (msg) => console.log(msg));
+socket.on("answer", async (answer) => {
+  console.log("Recieved the answer");
+  await myPeerConnection.setRemoteDescription(answer);
+}); // on peer A, when the answer is sent from B trough the server.
+
+// RTC Code
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+} // putting my video and audio inside the RTC, so that it is shared thru the server.
+
+function handleIce(data) {
+  console.log("got icecandidate");
+  console.log(data);
+}
--- src/server.js
+++ src/server.js
@@ -15,91 +15,22 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app); //this gives me the server
-const ioServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-instrument(ioServer, {
-  auth: false,
-  mode: "development",
-});
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = ioServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return ioServer.sockets.adapter.rooms.get(roomName)?.size;
-}
+const ioServer = new Server(httpServer);
 
 ioServer.on("connection", (socket) => {
-  socket["nickname"] = "Anonymous";
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, roomCreated) => {
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    roomCreated();
-    socket
-      .to(roomName)
-      .emit("welcome event", socket.nickname, countRoom(roomName));
-    ioServer.sockets.emit("room_change", publicRooms());
+
+    socket.to(roomName).emit("welcome");
   });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
   });
-  socket.on("disconnect", () => {
-    ioServer.sockets.emit("room_change", publicRooms());
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
   });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
-//done() does not run the backendDone() function at the backend. instead, it is just like pressing 'play' button that triggers backendDone() to be executed in the front-end.
-//and the done() can even send the argument.
 
-/*
-?*
-const wss = new WebSocket.Server({ server }); //creates websocket server on top of the http server. this allows the port can handle both the http:// and the ws://
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-  console.log("connected to browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌")); //when the browser tab is closed.
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  }); //listener for the message from the browser
-}); //this send 'message' to the socket
-
-//wss.on waits the event to happen, then the function is called. also it gives us the info of someone who just connected to our backend which is through the 'socket'.
-*/
 const handlelisten = () => console.log(`Listeneing on http://localhost:9000`); //express handles http
+
 httpServer.listen(9000, handlelisten);
